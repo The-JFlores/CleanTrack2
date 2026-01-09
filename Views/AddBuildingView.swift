778e02befe9c0844d45c7262261881c61a6f009d
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-01-09 11:53:46 -0500

    Add AddBuildingView presentation to HomeView with ability to add new buildings dynamically.

---
+++ Views/AddBuildingView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBuildingView.swift
+//  CleanTrack2.0
+//
+//  Created by Jose Flores on 2026-01-09.
+//
+
+import SwiftUI
+
+struct AddBuildingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBuildingView()
+}
