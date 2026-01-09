2687022377cf7006094fe3ffd78b932c99c49e16
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-01-09 09:08:50 -0500

    Initial Commit

---
+++ CleanTrack2.0/CleanTrack2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  CleanTrack2_0App.swift
+//  CleanTrack2.0
+//
+//  Created by Jose Flores on 2026-01-09.
+//
+
+import SwiftUI
+
+@main
+struct CleanTrack2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
